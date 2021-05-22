c492fb06966ff068ddaaf64518adba36626a4557
David Hundley <david.k.hundley@gmail.com>
2021-05-22 11:07:07 -0500

    Updating lifecycle configuration scripts

--- Part 2a - Creating a SageMaker Notebook/scripts/On Create/on-create.sh
+++ Part 2a - Creating a SageMaker Notebook/scripts/On Create/on-create.sh
@@ -2,39 +2,36 @@
 
 set -e
 
-# OVERVIEW
-# This script installs a custom, persistent installation of conda on the Notebook Instance's EBS volume, and ensures
-# that these custom environments are available as kernels in Jupyter.
-# 
-# The on-create script downloads and installs a custom conda installation to the EBS volume via Miniconda. Any relevant
-# packages can be installed here.
-#   1. ipykernel is installed to ensure that the custom environment can be used as a Jupyter kernel   
-#   2. Ensure the Notebook Instance has internet connectivity to download the Miniconda installer
+## CUSTOM KERNEL CREATION
+## ----------------------------------------------------------------
 
+# Setting the proper user credentials
 sudo -u ec2-user -i <<'EOF'
 unset SUDO_UID
 
-# Install a separate conda installation via Miniconda
+# Installing a separate conda installation via Miniconda
 WORKING_DIR=/home/ec2-user/SageMaker/custom-miniconda
 mkdir -p "$WORKING_DIR"
 wget https://repo.anaconda.com/miniconda/Miniconda3-4.6.14-Linux-x86_64.sh -O "$WORKING_DIR/miniconda.sh"
 bash "$WORKING_DIR/miniconda.sh" -b -u -p "$WORKING_DIR/miniconda" 
 rm -rf "$WORKING_DIR/miniconda.sh"
 
-
-# Create a custom conda environment
+# Instantiating variables used to create the custom kernel
 source "$WORKING_DIR/miniconda/bin/activate"
 KERNEL_NAME="dkhundley_custom_kernel"
 PYTHON="3.8"
 
+# Creating the custom kernel
 conda create --yes --name "$KERNEL_NAME" python="$PYTHON"
 conda activate "$KERNEL_NAME"
 
+# Installing the default ipykernel required from SageMaker
 pip install --quiet ipykernel
 
-# Customize these lines as necessary to install the required packages
-
 # Downloading the requirements.txt script from GitHub
+wget https://raw.githubusercontent.com/dkhundley/terraform-sagemaker-tutorial/main/Part%202a%20-%20Creating%20a%20SageMaker%20Notebook/scripts/On%20Create/requirements.txt
+
+# Installing the Python libraries listed in the requirements.txt script
 pip install --quiet -r requirements.txt
 
 EOF
--- Part 2a - Creating a SageMaker Notebook/scripts/On Start/on-start.sh
+++ Part 2a - Creating a SageMaker Notebook/scripts/On Start/on-start.sh
@@ -2,8 +2,11 @@
 
 set -e
 
+## IDLE AUTOSTOP STEPS
+## ----------------------------------------------------------------
+
 # Setting the timeout (in seconds) for how long the SageMaker notebook can run idly before being auto-stopped
-IDLE_TIME = 3600
+IDLE_TIME=3600
 
 # Getting the autostop.py script from GitHub
 echo "Fetching the autostop script..."
@@ -12,3 +15,26 @@ wget https://raw.githubusercontent.com/dkhundley/terraform-sagemaker-tutorial/ma
 # Using crontab to autostop the notebook when idle time is breached
 echo "Starting the SageMaker autostop script in cron."
 (crontab -l 2>/dev/null; echo "*/5 * * * * /usr/bin/python $PWD/autostop.py --time $IDLE_TIME --ignore-connections") | crontab -
+
+
+
+## CUSTOM CONDA KERNEL USAGE STEPS
+## ----------------------------------------------------------------
+
+# Setting the proper user credentials
+sudo -u ec2-user -i <<'EOF'
+unset SUDO_UID
+
+# Setting the source for the custom conda kernel
+WORKING_DIR=/home/ec2-user/SageMaker/custom-miniconda
+source "$WORKING_DIR/miniconda/bin/activate"
+
+# Loading all the custom kernels
+for env in $WORKING_DIR/miniconda/envs/*; do
+    BASENAME=$(basename "$env")
+    source activate "$BASENAME"
+    python -m ipykernel install --user --name "$BASENAME" --display-name "Custom ($BASENAME)"
+	done
+
+# Restarting the Jupyter server
+restart jupyter-server

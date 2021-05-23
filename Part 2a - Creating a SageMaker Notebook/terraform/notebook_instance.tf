1180efd2f67c877d89414dbb7f367a71c03d1eef
David Hundley <david.k.hundley@gmail.com>
2021-05-22 19:46:42 -0500

    Attaching lifecycle config to notebook

--- Part 2a - Creating a SageMaker Notebook/terraform/notebook_instance.tf
+++ Part 2a - Creating a SageMaker Notebook/terraform/notebook_instance.tf
@@ -3,5 +3,6 @@ resource "aws_sagemaker_notebook_instance" "notebook_instance" {
   name = "ds-quick-tips-instance"
   role_arn = aws_iam_role.notebook_iam_role.arn
   instance_type = "ml.t2.medium"
+  lifecycle_config_name = aws_sagemaker_notebook_instance_lifecycle_configuration.notebook_config.name
   default_code_repository = aws_sagemaker_code_repository.git_repo.code_repository_name
 }

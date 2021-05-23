d539f504a09e8a4a52fbca9cf280f39970f664ca
David Hundley <david.k.hundley@gmail.com>
2021-05-22 19:16:37 -0500

    Updating SM notebook terraform scripts

---
+++ Part 2a - Creating a SageMaker Notebook/terraform/iam.tf
@@ -0,0 +1,32 @@
+## DATA BLOCKS
+## ----------------------------------------------------------------
+
+# Defining the SageMaker "Assume Role" policy
+data "aws_iam_policy_document" "sm_assume_role_policy" {
+	statement {
+		actions = ["sts:AssumeRole"]
+		
+		principals {
+			type = "Service"
+			identifiers = ["sagemaker.amazonaws.com"]
+		}
+	}
+}
+
+
+
+## RESOURCE BLOCKS
+## ----------------------------------------------------------------
+
+# Defining the SageMaker notebook IAM role
+resource "aws_iam_role" "notebook_iam_role" {
+	name = "sm_notebook_role"
+	assume_role_policy = data.aws_iam_policy_document.sm_assume_role_policy.json
+}
+
+# Attaching the AWS default policy, "AmazonSageMakerFullAccess"
+resource "aws_iam_policy_attachment" "sm_full_access_attach" {
+	name = "sm-full-access-attachment"
+	roles = aws_iam_role.notebook_iam_role.name
+	policy_arn = "arn:aws:iam::aws:policy/AmazonSageMakerFullAccess"
+}
